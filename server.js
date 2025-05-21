d1d394d8f9afe5226f23e9ce552394464d09d9a5
junseoyook <airborne60@naver.com>
2025-05-21 20:18:32 +0900

    오류수정

--- server.js
+++ server.js
@@ -291,7 +291,7 @@ async function sendKakaoNotification(phoneNumber, parkingToken) {
           pfId: SOLAPI_PFID,
           templateId: "KA01TP250418063541272b3uS4NHhfLo",
           variables: {
-            "#{customerName}": ,
+            "#{customerName}": "",
             "#{pariking Url}": parkingUrl,
             "#{entry Url}": "비밀번호: 1234*",
             "#{checkInTime}": checkInTime,
@@ -805,7 +805,7 @@ async function generateBothTokens(phoneNumber, checkInDate, checkOutDate) {
           pfId: SOLAPI_PFID,
           templateId: "KA01TP250418063541272b3uS4NHhfLo",
           variables: {
-            "#{customerName}": "고객님",
+            "#{customerName}": "",
             "#{parkingUrl}": parkingUrl,
             "#{doorUrl}": doorUrl,
             "#{checkInTime}": checkInTime,
