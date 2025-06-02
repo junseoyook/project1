c5e2a4a41103171c5b29270637da66acbf4d27e1
junseoyook <airborne60@naver.com>
2025-06-02 19:55:41 +0900

    리모컨 페이지 연결

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 // 셀프 발급 예시 엔드포인트
 router.post('/issue', (req, res) => {
-  res.json({ success: true, message: '리모컨 URL 발급됨', url: '/customer/index.html' });
+  // 실제로는 동적으로 토큰을 생성해야 하지만, 예시로 고정값 사용
+  const token = 'testtoken123';
+  res.json({
+    success: true,
+    message: '리모컨 URL 발급됨',
+    parkingUrl: `/parking.html?token=${token}`
+  });
 });
 
 module.exports = router; 
