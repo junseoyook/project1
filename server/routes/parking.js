3127f14dd72a112fc504341c275c060be6483813
junseoyook <airborne60@naver.com>
2025-06-03 22:57:22 +0900

    라우터 문제 해결

--- server/routes/parking.js
+++ server/routes/parking.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const deviceController = require('../controllers/deviceController');
 
 // 차단기 열기
 router.post('/open', (req, res) => {
+  const device = deviceController.getDevice('ESP32_001');
+  if (!device) return res.status(404).json({ success: false, message: '디바이스 없음' });
+  deviceController.openGate(device);
   res.json({ success: true, message: '차단기 열기 명령 전송됨' });
 });
 
 // 차단기 닫기
 router.post('/close', (req, res) => {
+  const device = deviceController.getDevice('ESP32_001');
+  if (!device) return res.status(404).json({ success: false, message: '디바이스 없음' });
+  deviceController.closeGate(device);
   res.json({ success: true, message: '차단기 닫기 명령 전송됨' });
 });
 
